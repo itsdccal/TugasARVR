dd97bf492a6effacf691f9870dfc3528849a76aa
andi muh haikal lukman <113303662+itsdccal@users.noreply.github.com>
2024-09-23 15:50:53 +0800

    Initial commit

---
+++ Assets/Script/ChangeColor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public void Red()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
---
+++ Assets/Script/RemoveObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveObject : MonoBehaviour
+{
+    public int jarakRaycast = 20;
+    private RaycastHit raycastHit;
+
+    void Start()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        if (Physics.Raycast(ray, out raycastHit, jarakRaycast))
+        {
+            if (raycastHit.transform.CompareTag("Benda"))
+            {
+                Destroy(raycastHit.transform.gameObject);
+            }
+        }
+    }
+}
